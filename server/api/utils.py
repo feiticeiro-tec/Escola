1202440cb86c7f70cdb4ed529fe3088aa4329368
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-28 18:37:14 +0000

    add utils select_users_group

--- server/api/utils.py
+++ server/api/utils.py
@@ -1,6 +1,9 @@
-from ..database.models import Grupo
+from ..database.models import Grupo,Usuario
 
 def select_grupos(id=None):
     if id:
         return Grupo.query.get(id)
     return Grupo.query.all()
+
+def select_users_group(grupo_id):
+    return Usuario.query.filter(Usuario.grupo_id==grupo_id).all()
