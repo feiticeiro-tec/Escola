3cd6bcda6d1f854561d7eb6f766763ea7759e46f
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-28 17:30:37 +0000

    methods init_grpos

--- server/database/models/grupo.py
+++ server/database/models/grupo.py
@@ -1,8 +1,21 @@
 from .default import db, Default
 
-
 class Grupo(db.Model, Default):
     __tablename__ = 'Grupo'
 
-    def __init__(self, descricao):
-        super().__init__(descricao=descricao)
+    def __init__(self,id, descricao):
+        super().__init__(id=id,descricao=descricao)
+
+    def init_grupos():
+        try:
+            db.session.add_all([
+                Grupo(1,"ADM"),
+                Grupo(2,"DIRETOR"),
+                Grupo(3,"PROFESSOR"),
+                Grupo(4,"ALUNO")
+            ])
+            db.session.commit()
+        except:
+            db.session.rollback()
+        
+
