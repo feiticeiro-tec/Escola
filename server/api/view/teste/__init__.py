08a0e50d63ba1caed892190e9bfc935ffd7ac1d1
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-28 20:03:32 +0000

    get ids

--- server/api/view/teste/__init__.py
+++ server/api/view/teste/__init__.py
@@ -25,31 +25,31 @@ class Teste(Resource):
 @np_teste.route('/<int:teste_id>',methods=['GET','POST'])
 @np_teste.route('/<int:teste_id>/<int:pergunta_id>',methods=['GET','PUT','DELETE'])
 class TestePergunta(Resource):
-    def post(self):
+    def post(self,teste_id):
         """Deve criar uma nova pergunta"""
         ...
-    def get(self,pergunta_id=None):
+    def get(self,teste_id,pergunta_id=None):
         """Deve retorna uma lista de perguntas filtradas ou n"""
         ...
-    def put(self,pergunta_id):
+    def put(self,teste_id,pergunta_id):
         """Deve Atualizar a pergunta"""
         ...
-    def delete(self,pergunta_id):
+    def delete(self,teste_id,pergunta_id):
         """Deletar uma pergunta"""
         ...
 
 @np_teste.route('/<int:teste_id>/<int:pergunta_id>',methods=['GET','POST'])
 @np_teste.route('/<int:teste_id>/<int:pergunta_id>/<int:resposta_id>',methods=['GET','PUT','DELETE'])
 class TesteResposta(Resource):
-    def post(self):
+    def post(self,teste_id,pergunta_id):
         """Deve criar uma nova resposta"""
         ...
-    def get(self,resposta_id=None):
+    def get(self,teste_id,pergunta_id,resposta_id=None):
         """Deve retorna uma lista de respostas filtradas ou n"""
         ...
-    def put(self,resposta_id):
+    def put(self,teste_id,pergunta_id,resposta_id):
         """Deve Atualizar a pergunta"""
         ...
-    def delete(self,resposta_id):
+    def delete(self,teste_id,pergunta_id,resposta_id):
         """Deletar uma pergunta"""
         ...
