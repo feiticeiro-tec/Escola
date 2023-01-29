73875ad5641c8457532cd094bd9dbde1354bd3cd
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-29 18:57:40 +0000

    add methods delete an update on grupo alvo

--- server/database/models/grupo_alvo.py
+++ server/database/models/grupo_alvo.py
@@ -4,5 +4,14 @@ from .default import db, Default
 class GrupoAlvo(db.Model, Default):
     __tablename__ = 'GrupoAlvo'
 
-    def __init__(self, id, descricao):
-        super().__init__(id=id, descricao=descricao)
+    def __init__(self, descricao):
+        super().__init__(descricao=descricao)
+
+    def update(self, data):
+        for key, valor in data.items():
+            if valor:
+                setattr(self, key, valor)
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
