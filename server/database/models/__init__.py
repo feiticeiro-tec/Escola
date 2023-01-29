0c090d29d1cf2ec8be0d79182b4102757c0e53aa
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-29 19:01:10 +0000

    import GrupoAlvoUser

--- server/database/models/__init__.py
+++ server/database/models/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['Aluno', 'Sala', 'Usuario', 'Grupo', 'GrupoAlvo']
+__all__ = ['Aluno', 'Sala', 'Usuario', 'Grupo', 'GrupoAlvo', 'GrupoAlvoUser']
 from .aluno import Aluno
 from .sala import Sala
 from .usuario import Usuario
 from .grupo import Grupo
 from .grupo_alvo import GrupoAlvo
+from .grupo_alvo_user import GrupoAlvoUser
