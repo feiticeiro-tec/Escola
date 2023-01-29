dc1e2944e37084d7b63536c0f6dab6ac6a79ed58
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-29 19:15:38 +0000

    import decorador

--- server/api/decoradores/__init__.py
+++ server/api/decoradores/__init__.py
@@ -1,2 +1,3 @@
 from .usuario import validate_user, validate_user_path
 from .grupo_alvo import valdiate_grupo_alvo, valdiate_grupo_alvo_path
+from grupo_alvo_user import valdiate_grupo_alvo_user, valdiate_grupo_alvo_user_path
