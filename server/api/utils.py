afd04f0df3aa835d1b28600563125a6922eece26
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2023-01-29 18:39:54 +0000

    add select_group_alvo

--- server/api/utils.py
+++ server/api/utils.py
@@ -1,9 +1,17 @@
-from ..database.models import Grupo,Usuario
+from ..database.models import Grupo, Usuario, GrupoAlvo
+
 
 def select_grupos(id=None):
     if id:
         return Grupo.query.get(id)
     return Grupo.query.all()
 
+
 def select_users_group(grupo_id):
-    return Usuario.query.filter(Usuario.grupo_id==grupo_id).all()
+    return Usuario.query.filter(Usuario.grupo_id == grupo_id).all()
+
+
+def select_grupo_alvo(id=None):
+    if id:
+        return GrupoAlvo.query.get(id)
+    return GrupoAlvo.query.all()
